b7c468dfd13a6f5f413052b2b16099a26575bf18
Frederick Ayala <6177952+frederickayala@users.noreply.github.com>
2019-04-07 12:37:49 +0300

    Fixed line endings

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-cp ~/.vagrant.d/insecure_private_key .
-
+# Copy the key for ssh to the VMs
+cp ~/.vagrant.d/insecure_private_key . ;
+# Create the VMs
 for i in `seq -f '%02g' 1 $1`;
 do
   vagrant up c74$i
